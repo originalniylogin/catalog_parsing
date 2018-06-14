74ef9556fde1172618ec4dfe003e5fc35ad97956
Ilya Khachikyan <i.hachikyan@elonsoft.ru>
2018-06-14 16:07:07 +0300

    Change detail structure

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const       _ = require('lodash');
 const      fs = require('fs');
 
 const catalogURL = 'https://agrocat.ru/catalog/';
-const knownBrands = JSON.parse(fs.readFileSync('brands.json'));
+let brands = [];
 
-let matchedBrands = [];
+function saveDetails(brandName, details, fileName) {
+  const catalog = JSON.stringify({
+    machinery_brand_name: brandName,
+    parts: details
+  });
+  fs.writeFileSync(`catalogs/${brandName}/${fileName}.json`, catalog);
+}
 
 osmosis
   .get(catalogURL)
@@ -14,36 +20,29 @@ osmosis
     title: '.title',
     href: '@href'
   })
-  .data(brand => {
-    knownBrand = _.find(knownBrands, knownBrand => _.toLower(knownBrand.name) == _.toLower(brand.title))
-    if (knownBrand)
-      matchedBrands.push(_.merge(knownBrand, {href: brand.href}));
-  })
+  .data(brand => brands.push(brand))
   .done(() => {
-    matchedBrands.forEach(brand => {
+    brands.forEach(brand => {
+      const brandPath = `catalogs/${brand.title}`;
       let details = [];
+      let catalogsCounter = 0;
+      fs.existsSync(brandPath) || fs.mkdirSync(brandPath);
       osmosis
         .get(catalogURL + brand.href.replace('./', ''))
         .paginate('.navigation-line:first strong + a[href]')
         .find('.shop-item')
         .set({
-          catalogNumber: 'td:nth-child(1)',
+          catalog_number: 'td:nth-child(1)',
           name: 'td:nth-child(2)'
         })
-        .data(detail => {
-            details.push(_.merge(detail, {
-              detailsMachineryBrandsAttributes: [
-                {
-                  machineryBrandId: brand.id,
-                  originality: 0,
-                  manufacturer: brand.title
-                }
-              ]
-            }));
-        })
-        .done(() => {
-          const catalog = JSON.stringify(details);
-          fs.writeFile(`catalogs/${brand.name}.json`, catalog);
+        .delay(5)
+        .data(detail => details.push(_.merge(detail, {originality: 'original', manufacturer: brand.title})))
+        .then(() => {
+          if (details.length >= 1000) {
+            saveDetails(brand.title, details, ++catalogsCounter);
+            details = [];
+            console.log(`${brand.title}: ${catalogsCounter * 1000} parts written`);
+          }
         });
     });
   });
