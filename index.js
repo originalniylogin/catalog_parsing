eb5dfe993775a728bd26388b37a792fefcf88705
Ilya Khachikyan <i.hachikyan@elonsoft.ru>
2018-06-20 20:08:22 +0300

    temp

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const osmosis = require('osmosis');
-const       _ = require('lodash');
-const      fs = require('fs');
+'use strict';
 
-const catalogURL = 'https://agrocat.ru/catalog/';
-let brands = [];
+const   axios = require('axios');
+const cheerio = require('cheerio');
 
-function saveDetails(brandName, details, fileName) {
-  const catalog = JSON.stringify({
-    machinery_brand_name: brandName,
-    parts: details
-  });
-  fs.writeFileSync(`catalogs/${brandName}/${fileName}.json`, catalog);
-}
+const catalogsURL = 'https://agrocat.ru/catalog/';
 
-osmosis
-  .get(catalogURL)
-  .find('.col-xs-6.col-sm-3.v-margin.text-center a')
-  .set({
-    title: '.title',
-    href: '@href'
-  })
-  .data(brand => brands.push(brand))
-  .done(() => {
-    brands.forEach(brand => {
-      const brandPath = `catalogs/${brand.title}`;
-      let details = [];
-      let catalogsCounter = 0;
-      fs.existsSync(brandPath) || fs.mkdirSync(brandPath);
-      osmosis
-        .get(catalogURL + brand.href.replace('./', ''))
-        .paginate('.navigation-line:first strong + a[href]')
-        .find('.shop-item')
-        .set({
-          catalog_number: 'td:nth-child(1)',
-          name: 'td:nth-child(2)'
-        })
-        .delay(5)
-        .data(detail => details.push(_.merge(detail, {originality: 'original', manufacturer: brand.title})))
-        .then(() => {
-          if (details.length >= 1000) {
-            saveDetails(brand.title, details, ++catalogsCounter);
-            details = [];
-            console.log(`${brand.title}: ${catalogsCounter * 1000} parts written`);
-          }
-        });
+axios.get(catalogsURL)
+  .then(response => {
+    const catalogsPage = cheerio.load(response.data);
+
+    const brands = catalogsPage('.col-xs-6.col-sm-3.v-margin.text-center a').map((i, brand) =>
+      ({
+        title: catalogsPage(brand).find('.title').text(),
+        href: catalogsPage(brand).attr('href')
+      })
+    ).get();
+    
+    brands.forEach(async brand => {
+      await scrapBrand(brand, 1);
     });
+  })
+  .catch(error => {
+    console.log(error);
   });
+
+async function scrapBrand(brand, page) {
+  const brandURL = catalogsURL + brand.href.slice(2) + `?page=${page}`;
+  let parts = [];
+
+  await axios.get(brandURL)
+    .then(brandResponse => {
+      const brandPage = cheerio.load(brandResponse.data);
+      const parts = brandPage('.shop-item').map((i, part) => {
+        const catalogNumber = brandPage(part).find('td').get(0);
+        const name = brandPage(part).find('td').get(1);
+        return {
+          catalog_number: brandPage(catalogNumber).text(),
+          name: brandPage(name).text()
+        };
+      }).get();
+
+      console.log(parts);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
