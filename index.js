7e388220bfe73a465b3190dfd469e731dbb87138
originalniylogin <il007770@gmail.com>
2018-06-21 04:28:40 +0300

    Rewrite the whole thing

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const   axios = require('axios');
-const cheerio = require('cheerio');
+const          _ = require('lodash');
+const         fs = require('fs');
+const      axios = require('axios');
+const    cheerio = require('cheerio');
+const axiosRetry = require('axios-retry');
 
 const catalogsURL = 'https://agrocat.ru/catalog/';
+
+axiosRetry(
+  axios,
+  {
+    retries: 10,
+    retryDelay: (retryCount) => {
+      return retryCount * 1000;
+    }
+  }
+);
 
 axios.get(catalogsURL)
   .then(response => {
@@ -11,36 +24,64 @@ axios.get(catalogsURL)
 
     const brands = catalogsPage('.col-xs-6.col-sm-3.v-margin.text-center a').map((i, brand) =>
       ({
-        title: catalogsPage(brand).find('.title').text(),
-        href: catalogsPage(brand).attr('href')
+        data: {
+          title: catalogsPage(brand).find('.title').text(),
+          href: catalogsPage(brand).attr('href')
+        },
+        count: 0
       })
     ).get();
     
     brands.forEach(async brand => {
-      await scrapBrand(brand, 1);
+      fs.existsSync(`catalogs/${brand.data.title}`) || fs.mkdirSync(`catalogs/${brand.data.title}`);
+      
+      let page = 1;
+      while (true) {
+        if (brand.data.title != 'Шины') break;
+        const parts = await scrapPage(brand.data, page);
+        if (parts == undefined)
+          break;
+
+        if (parts.length > 0) {
+          const catalog = JSON.stringify({
+            machinery_brand_name: brand.data.title,
+            parts: parts
+          });
+          fs.writeFileSync(`catalogs/${brand.data.title}/${page}.json`, catalog);
+        }
+
+        brand.count += parts.length;
+        console.log(`${brand.data.title} page ${page++}: ${parts.length} parts writen. total: ${brand.count}`);
+      }
+      fs.writeFileSync(`catalogs/${brand.data.title}/total.json`, JSON.stringify({totalCount: brand.count}));
     });
   })
   .catch(error => {
     console.log(error);
   });
 
-async function scrapBrand(brand, page) {
+async function scrapPage(brand, page) {
   const brandURL = catalogsURL + brand.href.slice(2) + `?page=${page}`;
   let parts = [];
 
-  await axios.get(brandURL)
+  return await axios.get(brandURL)
     .then(brandResponse => {
       const brandPage = cheerio.load(brandResponse.data);
-      const parts = brandPage('.shop-item').map((i, part) => {
-        const catalogNumber = brandPage(part).find('td').get(0);
-        const name = brandPage(part).find('td').get(1);
-        return {
-          catalog_number: brandPage(catalogNumber).text(),
-          name: brandPage(name).text()
+      if (brandPage('.navigation-line strong + a').html() == null)
+        return undefined;
+
+      const parts = brandPage('.shop-item').map((i, row) => {
+        const catalogNumber = brandPage(brandPage(row).find('td').get(0)).text();
+        const name = brandPage(brandPage(row).find('td').get(1)).text();
+        const part = {
+          catalog_number: catalogNumber,
+          name: name
         };
+
+        return _.includes(_.values(part), '') ? null : part;
       }).get();
 
-      console.log(parts);
+      return _.without(parts, null);
     })
     .catch(error => {
       console.log(error);
